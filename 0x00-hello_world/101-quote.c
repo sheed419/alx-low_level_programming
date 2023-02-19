e0fcd477754c3a03e427db53fea88e261665ea53
sheed419 <realhighkage@gmail.com>
2023-02-19 11:23:54 -0800

    dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main(void){
 char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, 59);
 return (1);
