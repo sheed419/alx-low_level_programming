6ebbe27852ef1d8cebc98a7b577d99b05825c988
sheed419 <realhighkage@gmail.com>
2023-02-19 07:35:59 -0800

    diq measuring contest

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 printf("Size of char: %ld byte(s)\n", sizeof(char));
 printf("Size of long: %ld byte(s)\n", sizeof(long));
 printf("Size of short: %ld byte(s)\n", sizeof(short));
-printf("Size of int: %ld byte(s)\n", sizeof(int);
+printf("Size of int: %ld byte(s)\n", sizeof(int));
 printf("Size of float: %ld byte(s)\n", sizeof(float));
 printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-printf("Size of void: %ld byte(s)\n", sizeof(void));
+printf("Size of void: %ld byte(s)\n", sizeof(void*));
 printf("Size of double: %ld byte(s)\n", sizeof(double));
 printf("Size of long double: %ld byte(s)\n", sizeof(long double));
 return (0);
