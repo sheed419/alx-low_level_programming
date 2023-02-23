200e67b79d0fa75d6f4a369298ef06b21fb61ba5
sheed419 <realhighkage@gmail.com>
2023-02-23 07:46:45 -0800

    reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints the lowercase alphabet in reverse, followed by a new line
+ * Description:Prints the lowercase alphabet in reverse followed by a new line
  *
  * Return: Always 0 (Success)
  */
@@ -10,13 +10,10 @@ int main(void)
 {
 char letter = 'z';
 
-do 
-{
+do{
 putchar(letter);
 letter--;
-} 
-while (letter >= 'a');
-
+} while (letter >= 'a');
 putchar('\n');
 
 return (0);
